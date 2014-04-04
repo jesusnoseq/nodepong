f0e67c873fb1a7f4d3c5fcfaca2716781b90ccae
Jesús Rodríguez Pérez <jesusnoseq@hotmail.com>
2014-04-05 01:11:49 +0200

    Esqueleto de la aplicacion

--- app/nodepong.js
+++ app/nodepong.js
@@ -1,39 +1,64 @@
 var express = require("express");
 var app = express();
 var port = 80;
- 
 
 //app.set('port',process.env.PORT || 80);
 
 app.set('view engine', "jade");
 app.engine('jade', require('jade').__express);
 
-
 app.set('views', __dirname + '/views');
-app.use(express.static(__dirname + '/public')); 
-
+app.use(express.static(__dirname + '/public'));
 
 app.use(express.cookieParser());
-app.use(express.session({ secret: 'esto es secreto'}));
+app.use(express.session({
+	secret : 'esto es secreto'
+}));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 
-
-
-app.get("/", function(req, res){
-    res.render("index");
+app.get("/", function(req, res) {
+	res.render("index");
 });
-
 
 var io = require('socket.io').listen(app.listen(port));
 
-io.sockets.on('connection', function (socket) {
-	
-  socket.emit('news', { hello: 'world' });
-  
-  socket.on('play', function (data) {
-    console.log(data);
-  });
+/*
+io.configure('production', function(){
+  io.enable('browser client etag');
+  io.set('log level', 1);
+
+  io.set('transports', [
+    'websocket',
+    'flashsocket'
+  ]);
+});*/
+process.env.NODE_ENV = 'production';
+io.configure('development', function(){
+  io.set('transports', ['websocket']);
 });
 
-console.log("Listening on port " + port);
+var playerIDCounter = 0;
+var fps = 30;
+var speed = 10;
+
+
+io.sockets.on('connection', function(socket) {
+	//socket.set('id',playerIDCounter);
+	playerIDCounter++;
+	
+	
+	socket.emit('draw', {
+		//id : socket.get('id')
+	});
+
+	socket.on('play', function(data) {
+		console.log(data);
+	});
+	
+	socket.on('disconnect', function() {
+		io.sockets.emit('user disconnected');
+	});
+});
+
+console.log("Listening on port " + port); 
--- app/public/pong.js
+++ app/public/pong.js
@@ -1,11 +1,27 @@
 window.onload = (function() {
 
-	var socket = io.connect('http://localhost');
-	socket.on('news', function(data) {
-		console.log(data);
-		socket.emit('play', {
-			my : 'data'
-		});
+	var socket = io.connect('http://localhost:80');
+
+	socket.on('connect', function() {
+		socket.send();
+		console.log('Connected!');
+		
+		
+		//player = new Player(50, 50);
 	});
 
-}); 
+	socket.on('draw', function(data) {
+		console.log(data);
+	});
+
+	socket.on('disconnect', function() {
+		console.log('Disconnected!');
+	});
+
+});
+
+function keyPress(key) {
+	socket.emit('keypress', {
+		keypress : key
+	});
+}
