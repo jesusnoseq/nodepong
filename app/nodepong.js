f32ea32fdaeae7940cda56eceb25ca28508d54a3
Jesús Rodríguez Pérez <jesusnoseq@hotmail.com>
2014-04-06 23:38:23 +0200

    Definiendo objetos y motor de juego

--- app/nodepong.js
+++ app/nodepong.js
@@ -24,30 +24,84 @@ app.get("/", function(req, res) {
 var io = require('socket.io').listen(app.listen(port));
 
 /*
-io.configure('production', function(){
-  io.enable('browser client etag');
-  io.set('log level', 1);
+ io.configure('production', function(){
+ io.enable('browser client etag');
+ io.set('log level', 1);
 
-  io.set('transports', [
-    'websocket',
-    'flashsocket'
-  ]);
-});*/
-process.env.NODE_ENV = 'production';
-io.configure('development', function(){
-  io.set('transports', ['websocket']);
+ io.set('transports', [
+ 'websocket',
+ 'flashsocket'
+ ]);
+ });*/
+
+process.env.NODE_ENV = 'development';
+io.configure('development', function() {
+	io.set('transports', ['websocket']);
 });
 
 var playerIDCounter = 0;
-var fps = 30;
+var FPS = 30;
 var speed = 10;
+var scenaryWithd=1000;
+var scenaryHeight=500;
+
+var Actor = (function(velocidad, xIni, yIni) {
+	this.v = velocidad;
+	this.x = xIni;
+	this.y = yIni;
+});
+
+var Jugador=(function() {
+
+	this.mover=(function (dir) {
+		switch(dir) {
+			case 1:
+				this.y+=this.v;
+				break;
+			case 2:
+				this.y-=this.v;
+				break;
+			default:
+
+		}
+	});
+});
+
+var Bola=(function(angulo) {
+	this.angulo=angulo;// en radianes
+
+	this.mover = (function() {
+		vy = (float)(v * Math.sin(angulo));
+		vx = (float)(v * Math.cos(angulo));
+		x += vx;
+		y -= vy;
+	});
+
+	this.chocar = (function() {
+
+	});
+});
+
+Jugador.prototype = new Actor();
+Bola.prototype = new Actor();
+
+function update(){
+	
+}
+
+
+
+setInterval(function() {
+    update();
+    player.draw();
+}, 1000/FPS);
+
 
 
 io.sockets.on('connection', function(socket) {
 	//socket.set('id',playerIDCounter);
 	playerIDCounter++;
-	
-	
+
 	socket.emit('draw', {
 		//id : socket.get('id')
 	});
@@ -55,10 +109,11 @@ io.sockets.on('connection', function(socket) {
 	socket.on('play', function(data) {
 		console.log(data);
 	});
-	
+
 	socket.on('disconnect', function() {
 		io.sockets.emit('user disconnected');
 	});
 });
 
-console.log("Listening on port " + port); 
+
+console.log("Listening on port " + port);
