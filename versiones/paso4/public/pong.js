705220d107a63b4c7f9135b7a6a10464763558a3
Jesús Rodríguez Pérez <jesusnoseq@hotmail.com>
2014-04-19 17:09:21 +0200

    paso4

---
+++ versiones/paso4/public/pong.js
@@ -0,0 +1,90 @@
+var url="http://localhost:8080";
+
+var KEY_UP = 38;
+var KEY_DOWN = 40;
+var canvas = null, ctx = null;
+
+
+window.onload = (function() {
+	var socket = io.connect(url);
+	socket.on('connect', function() {
+		socket.emit('adduser',{hola: new Date()});
+	});
+});
+
+
+
+
+
+function init(data) {
+	canvas = document.getElementById('juego');
+	ctx = canvas.getContext('2d');
+
+	setFullScreen(data);
+	
+	paint(data);
+}
+
+
+
+function setFullScreen(data) {
+	canvas.width = data.width;
+	canvas.height = data.height;
+
+	var w = window.innerWidth / canvas.width;
+	var h = window.innerHeight / canvas.height;
+	var scale = Math.min(h, w);
+
+	//DEFINIR ANCHO Y ALTO DEL CANVAS
+	canvas.style.width = (canvas.width * scale) + 'px';
+	canvas.style.height = (canvas.height * scale) + 'px';
+
+	//CENTRAR PANTALLA
+	canvas.style.position = 'fixed';
+	canvas.style.left = '50%';
+	canvas.style.top = '50%';
+	canvas.style.marginLeft = -(canvas.width * scale) / 2 + 'px';
+	canvas.style.marginTop = -(canvas.height * scale) / 2 + 'px';
+}
+
+function paint(data) {		
+	//FONDO
+	ctx.fillStyle = '#000';
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+	//BARRA DEL JUGADOR 1
+	ctx.fillStyle = '#fff';
+	ctx.fillRect(10, 200, 15, 100);
+
+	//BARRA DEL JUGADOR 2
+	ctx.fillRect(975, 200, 15, 100);
+	
+	//BOLITA
+	ctx.fillRect(490, 240, 20, 20);
+
+	//RED
+	paintLet();
+	
+	//DATOS
+	paintData();
+}
+
+function paintLet()
+{
+	var i;
+	for(i=10;i<500;i+=20)
+	{
+		ctx.fillRect(495,i,10,10);
+	}
+}
+
+function paintData(data)
+{
+	ctx.font="75px Arial";
+	
+	//GOLES J1
+	ctx.fillText(0,390,75); 
+	
+	//GOLES J2
+	ctx.fillText(0,560,75); 
+}
